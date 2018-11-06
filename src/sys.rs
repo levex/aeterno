016ad9d0a9463ad842b28170b702d846c5c9cedf
Levente Kurusa <lkurusa@acm.org>
2018-11-06 20:05:37 +0100

    sys: basic command parsing
    Signed-off-by: Levente Kurusa <lkurusa@acm.org>

--- src/sys.rs
+++ src/sys.rs
@@ -5,10 +5,12 @@
  *   - Use helper for everything instead of risking pid 1 to crash
  */
 
-extern crate nix;
 use std::os::unix::io::RawFd;
+use std::str::from_utf8;
+
+extern crate nix;
 use nix::sys::socket::{accept, listen, MsgFlags, recv};
-use nix::unistd::{close};
+use nix::unistd::{close, write};
 
 #[macro_use]
 extern crate log;
@@ -20,15 +22,50 @@ const SYS_SOCKET_BACKLOG: usize = 5;
 
 use std::thread;
 
+#[derive(Debug, PartialEq, Eq)]
+enum Query {
+    Helo,
+    ProtocolError,
+}
+
+impl From<&str> for Query {
+    fn from(s: &str) -> Query {
+        match s {
+            "HELO\n" => Query::Helo,
+            _ => Query::ProtocolError,
+        }
+    }
+}
+
+fn reply_query(conn_fd: RawFd, q: Query) {
+    match q {
+        Query::Helo => {
+            info!("Received HELO from fd {:?}", conn_fd);
+        },
+        Query::ProtocolError => {
+            info!("Protocol error with fd {:?}", conn_fd);
+        },
+    }
+}
+
 fn handle_connection(conn_fd: RawFd) {
     debug!("Handling connection for FD {}", conn_fd);
     /* Read in command from the connection */
     let buf: &mut [u8] = &mut [0; 256];
     let size = recv(conn_fd, buf, MsgFlags::empty());
     if size.is_ok() {
-        debug!("Received {} bytes\n", size.unwrap());
+        debug!("Received {} bytes", size.unwrap());
+        let query = from_utf8(&buf)
+            .map(|str| { str.trim_matches(char::from(0)) })
+            .map(From::from)
+            .unwrap_or_else(|err| {
+                debug!("{:?}", err);
+                Query::ProtocolError
+            });
+
+        reply_query(conn_fd, query);
     } else {
-        debug!("Failed to receive from socket\n");
+        debug!("Failed to receive from socket");
     }
 
     let _ = close(conn_fd);
@@ -45,6 +82,7 @@ fn socket_listener() {
                 handle_connection(conn_fd);
             });
             debug!("FD {}: r = {:?}", conn_fd, r);
+            /* FIXME: FDs keep piling up here */
         }
     }
 }
